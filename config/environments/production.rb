ee26f674f027c36b4e613eb23f03d2b8964bd140
EdwardReed <edwardareed@gmail.com>
2015-03-15 16:57:20 -0700

    Update CORs Origin

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.middleware.insert_before 0, "Rack::Cors" do
     allow do
-      origins '*'
+      origins ENV['ORIGIN_URL']
       resource '*', :headers =>  :any, methods: [:get, :post, :options]
     end
   end
